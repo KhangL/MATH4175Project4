b216326ef2fe0a7d4d33f68e0cec6b5398d96da9
Harrison Gagnon <harrisong@cherry.rlogin>
2022-12-07 00:11:17 -0500

    Update q4 calculations. Issue with counter

--- p4.c
+++ p4.c
@@ -3,49 +3,45 @@
 
 #define M 6
 
-static const unsigned char S_box[2][8] = {{0x6, 0x5, 0x1, 0x0, 0x3, 0x2, 0x7, 0x4}, // I -> O
-                                          {0x3, 0x2, 0x5, 0x4, 0x7, 0x1, 0x0, 0x6}}; // O -> I
-static const unsigned char plaintext[] = {0x27, 0x07, 0x0C, 0x18, 0x08, 0x1A};
-static const unsigned char ciphertext[] = {0x24, 0x32, 0x39, 0x1D, 0x0D, 0x29};
+static const unsigned char S_box[2][8]  = {{0x6, 0x5, 0x1, 0x0, 0x3, 0x2, 0x7, 0x4}, // I -> O
+                                           {0x3, 0x2, 0x5, 0x4, 0x7, 0x1, 0x0, 0x6}}; // O -> I
+// static const unsigned char plaintext[]  = {0x07, 0x18, 0x1A};
+// static const unsigned char x_star[]     = {0x06, 0x19, 0x1B};
+// static const unsigned char ciphertext[] = {0x32, 0x1D, 0x29};
+// static const unsigned char y_star[]     = {0x36, 0x1F, 0x28};
+static const unsigned char ciphertext[] = {0x24, 0x39, 0x0D};
+static const unsigned char y_star[]     = {0x3E, 0x20, 0x03};
+static const unsigned char trails[]     = {0x20, 0x30, 0x38};
 
 int main()
 {
-    unsigned char counter[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    for (unsigned char i = 0x00; i < 0x06; i++)
+    unsigned char counter[] = {0, 0, 0};
+    for (unsigned char i = 0x00; i < 0x03; i++)
     {
-        printf("Round: %d, ", i);
-
-        // Grab Plaintext values
-        bool p1 = (plaintext[i] & 0x20) >> (M - 1); // Bitmask 0x20 (0010 0000)
-        bool p2 = (plaintext[i] & 0x10) >> (M - 2); // Bitmask 0x10 (0001 0000)
-        bool p4 = (plaintext[i] & 0x04) >> (M - 4); // Bitmask 0x04 (0000 0100)
-        bool p5 = (plaintext[i] & 0x02) >> (M - 5); // Bitmask 0x02 (0000 0010)
-
-        bool pSum = p1 ^ p2 ^ p4 ^ p5;
-        printf("pSum: %d\n", pSum); // Compute P1 ^ P2 ^ P4 ^ P5 ^ H1
- 
-
-        // Iterate through every key
+        printf("Trail: %d\n", i);
+        // Iterate through every key (0b000 - 0b111)
         for (unsigned char k = 0x00; k < 0x08; k++)
         {
-            printf("\tK: %d, ", k);
-            unsigned char J = (ciphertext[i] ^ (k << 3)) >> 3 & 0x07; // C ^ K = J
-            printf("J: 0x%01X, ", J);
+            printf("\tK = %d, ", k);
+            unsigned char J = ((k << 3) ^ ciphertext[i]) >> 3; // K ^ C = J 
+            printf("J = 0x%01X, ", J);
             unsigned char H = S_box[1][J]; // Unshuffle J = H
-            printf("H: %d, ", H);
-            printf("H1: %d, ", (H >> 2));
-            if (!(pSum ^ (H >> 2))) counter[k]++; // add pSum XOR H1 to the counter for K
-            printf("Sum %d: %d\n", k, pSum ^ (H >> 2));
+            printf("H = 0x%01X, ", H);
+            unsigned char J_star = ((k << 3) ^ y_star[i]) >> 3; // K ^ y* = J*
+            printf("J* = 0x%01X, ", J_star);
+            unsigned char H_star = S_box[1][J_star];
+            printf("H* = 0x%01X, ", H_star);
+            unsigned char H_prime = H ^ H_star;
+            printf("H' = 0x%01X\n", H_prime);
+            if (H_prime & trails[i]) counter[i]++;
         }
         printf("\n");
     }
 
     // Print counter values
-    for (unsigned char i = 0; i < 0x08; i++)
+    for (unsigned char i = 0; i < 0x03; i++)
     {
-        printf("Counter %u: %u\n", i, counter[i]);
+        printf("Counter %u: %u\n", i+1, counter[i]);
     }
     return 0;
 }
-
-main()
