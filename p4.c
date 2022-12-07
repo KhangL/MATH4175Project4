6a233ff2eca91462a470037c29e74b8fa17c7297
Harrison Gagnon <harrisong@cottonwood.rlogin>
2022-12-07 03:58:54 -0500

    Changed looping

--- p4.c
+++ p4.c
@@ -4,36 +4,52 @@
 
 static const unsigned char S_box[2][8]  = {{0x6, 0x5, 0x1, 0x0, 0x3, 0x2, 0x7, 0x4}, // I -> O
                                            {0x3, 0x2, 0x5, 0x4, 0x7, 0x1, 0x0, 0x6}}; // O -> I
-static const unsigned char ciphertext[] = {0x13, 0x2E, 0x25};
-static const unsigned char y_star[]     = {0x1B, 0x3E, 0x05};
-static const unsigned char trails[]     = {0x20, 0x30, 0x38};
+static const unsigned char ciphertext[] = {0x32, 0x1D, 0x29};
+static const unsigned char y_star[]     = {0x36, 0x1F, 0x28};
+static const unsigned char trails[]     = {0x1, 0x3, 0x7};
 static const float propRatios[] = {(float)1/4, (float)1/8, (float)1/8};
 
 int main()
 {
-    unsigned char counter[] = {0, 0, 0};
-    float C = 0;
+    unsigned char counter[] = {0, 0, 0, 0, 0, 0, 0, 0}; // Counter for each key
+    float C = 0; // Weighted average count 
+    // Iterate over the three trails
     for (unsigned char i = 0x00; i < 0x03; i++)
     {
-        printf("Trail: %d\n", i);
+        printf("Trail: %u\n", i+1);
+
         // Iterate through every key (0b000 - 0b111)
-        for (unsigned char k = 0x00; k < 0x08; k++)
-        {
-            printf("\tK = %d, ", k);
-            unsigned char J = (k ^ (ciphertext[i]>>3)); // K ^ C = J 
-            printf("J = 0x%01X, ", J);
-            unsigned char H = S_box[1][J]; // Unshuffle J = H
-            printf("H = 0x%01X, ", H);
-            unsigned char J_star = (k ^ (y_star[i]>>3)); // K ^ y* = J*
-            printf("J* = 0x%01X, ", J_star);
-            unsigned char H_star = S_box[1][J_star]; // Unshuffle J* = H*
-            printf("H* = 0x%01X, ", H_star);
-            unsigned char H_prime = H ^ H_star; // H + H* = H' 
-            printf("H' = 0x%01X\n", H_prime);
-            if (H_prime == (trails[i]>>3)) counter[i]++;
+        for (unsigned char j = 0; j < 0x03; j++) {
+            printf("\n\tY = 0x%02X, Y* = 0x%02X\n", ciphertext[j], y_star[j]);
+            for (unsigned char k = 0x00; k < 0x08; k++)
+            {
+                printf("\t\tK = %d, ", k);
+                unsigned char J = (k ^ (ciphertext[j] & 0x07)); // K ^ C = J 
+                printf("J = 0x%01X, ", J);
+                unsigned char H = S_box[1][J]; // Unshuffle J = H
+                printf("H = 0x%01X, ", H);
+                unsigned char J_star = (k ^ (y_star[j] & 0x07)); // K ^ y* = J*
+                printf("J* = 0x%01X, ", J_star);
+                unsigned char H_star = S_box[1][J_star]; // Unshuffle J* = H*
+                printf("H* = 0x%01X, ", H_star);
+                unsigned char H_prime = H ^ H_star; // H + H* = H' 
+                printf("H' = 0x%01X\n", H_prime);
+                if (H_prime == (trails[i])) counter[k]++;
+            }
         }
-        printf("Counter %u: %u\n", i+1, counter[i]);
-        C += propRatios[i] * counter[i];
+
+        // Prints Counter table
+        printf("\nCounter: %u\t", i+1);
+        for (unsigned char j = 0; j < 0x08; j++) {
+            printf("0x%1X ", j);
+        }
+        printf("\n         \t");
+        for (unsigned char j = 0; j < 0x08; j++) {
+            printf("%2u  ", counter[i]);
+        }
+        printf("\n\n");
+
+        C += propRatios[i] * counter[i]; // TODO: Fix calculation for weighted average
     }
 
     printf("C = %.04f", C);
